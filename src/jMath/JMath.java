9c355483e70c6d53a39a3b736e8295a47cf68a40
Landon Moon <lankm@live.com>
2019-09-11 19:06:30 -0500

    Formatted the JMath class

--- src/jMath/JMath.java
+++ src/jMath/JMath.java
@@ -2,22 +2,25 @@ package jMath;
 
 public class JMath {
     public static double round(double numb, int prec) {
-        numb*=Math.pow(10,prec);
-        numb=Math.round(numb)/Math.pow(10,prec);
+        numb *= Math.pow(10, prec);
+        numb = Math.round(numb) / Math.pow(10, prec);
         return numb;
     }
+
     public static <E extends Number, Comparable> double average(E... numbers) {
-        double total=0;
-        for(E number: numbers) {
-            total+=Double.parseDouble(number.toString());
+        double total = 0;
+        for (E number : numbers) {
+            total += Double.parseDouble(number.toString());
         }
-        return total/(numbers.length);
+        return total / (numbers.length);
     }
+
     public static boolean isOdd(int numb) {
-        if(numb%2==1)
+        if (numb % 2 == 1)
             return true;
         return false;
     }
+
     public static boolean isEven(int numb) {
         return !isOdd(numb);
     }
